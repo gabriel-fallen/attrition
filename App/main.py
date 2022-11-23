310801f5d727d056d6cc5e6e1d5399be80301d80
Alex Chichigin <sad.ronin@gmail.com>
2022-11-23 12:14:30 +0300

    Convert numbers to actual numbers

--- App/main.py
+++ App/main.py
@@ -13,22 +13,22 @@ def predict():
     # load and parse input
     data = request.json
     vector = [
-        data['age'],
+        float(data['age']),
         data['travel'],
         data['department'],
-        data['distance'],
-        data['education'],
+        float(data['distance']),
+        float(data['education']),
         data['gender'],
-        data['satisfaction'],
+        float(data['satisfaction']),
         data['maritalstatus'],
-        data['income'],
+        float(data['income']),
         data['overtime'],
-        data['totalyears'],
-        data['years'],
-        data['lastpromotion']
+        float(data['totalyears']),
+        float(data['years']),
+        float(data['lastpromotion'])
     ]
     # app.logging.info(f'vector: {vector}')
-    print(f'vector: {vector}\n')
+    # print(f'vector: {vector}\n')
 
     # load the model
     with open(os.path.join('data', 'logistic.pkcls'), 'rb') as file:
@@ -36,7 +36,7 @@ def predict():
 
     predictions = model(vector, 1)
     # app.logging.info(f'predictions: {predictions}')
-    print(f'predictions: {predictions}\n')
+    # print(f'predictions: {predictions}\n')
 
     # send the response
     return jsonify({ "predictions": { "leave": predictions[0], "stay": predictions[1] } })
